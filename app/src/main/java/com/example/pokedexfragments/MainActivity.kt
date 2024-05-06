03dd5613108b7541cd67b77258ab18d1729a0c97
agusCod3 <agus.romero.salazar@gmail.com>
2024-05-06 16:35:25 -0400

    Detail fragment listo

--- app/src/main/java/com/example/pokedexfragments/MainActivity.kt
+++ app/src/main/java/com/example/pokedexfragments/MainActivity.kt
@@ -5,23 +5,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.pokedexfragments.fragments.DetailPokemonFragment
 import com.example.pokedexfragments.fragments.ListPokemonFragment
 import com.example.pokedexfragments.model.Pokemon
 
 class MainActivity : AppCompatActivity() , ListPokemonFragment.PokemonSelectListener{
+
+    private lateinit var detailPokemonFragment: DetailPokemonFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        detailPokemonFragment = supportFragmentManager.findFragmentById(R.id.fragment_detail)
+                    as DetailPokemonFragment
+
+
     }
 
     override fun onPokemonSelected(pokemon: Pokemon) {
-
+            detailPokemonFragment.initUI(pokemon)
     }
 
     /*fun iAmYourFatherActivity(pokemon: Pokemon){
--- app/src/main/java/com/example/pokedexfragments/fragments/DetailPokemonFragment.kt
+++ app/src/main/java/com/example/pokedexfragments/fragments/DetailPokemonFragment.kt
@@ -6,15 +6,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.pokedexfragments.R
+import com.example.pokedexfragments.databinding.FragmentDetailPokemonBinding
+import com.example.pokedexfragments.databinding.FragmentListPokemonBinding
+import com.example.pokedexfragments.model.Pokemon
 
 class DetailPokemonFragment : Fragment() {
+
+    private lateinit var binding: FragmentDetailPokemonBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_pokemon, container, false)
+    ): View {
+        binding= FragmentDetailPokemonBinding.inflate(inflater,container,false)
+
+        return binding.root
+
+    }
+
+    fun initUI(pokemon: Pokemon){
+        binding.txtPokemonHp.text =getString(R.string.hp_format, pokemon.hp)
+        binding.txtPokemonAttack.text = getString(R.string.attack_format, pokemon.attack)
+        binding.txtPokemonDefense.text = getString(R.string.defense_format, pokemon.defense)
+        binding.txtPokemonVelocity.text = getString(R.string.velocity_format, pokemon.speed)
+
     }
 
 }
